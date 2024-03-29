2f9bb5c442864c330ebf8518304a43d54dcd6d5a
Yusuf Wibisono <yusuf.wibisono-guest@moladin.com>
2024-03-29 07:26:53 +0700

    add dummy data

---
+++ datasource/migrations/000000_medical_records_dummy.sql
@@ -0,0 +1,8 @@
+INSERT INTO.health_services (id, name, image_url, created_at, updated_at)
+VALUES 
+    (DEFAULT, 'Dokter Umum', null, DEFAULT, DEFAULT),
+    (DEFAULT, 'Spesialis Penyakit Dalam', null, DEFAULT, DEFAULT),
+    (DEFAULT, 'Spesialis Gizi', null, DEFAULT, DEFAULT),
+    (DEFAULT, 'Spesialis Kandungan', null, DEFAULT, DEFAULT),
+    (DEFAULT, 'Dokter Gigi', null, DEFAULT, DEFAULT),
+    (DEFAULT, 'Seksologis', null, DEFAULT, DEFAULT)
