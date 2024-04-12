eb20855da84b7b95e245a32939fe1aae5079406a
Yusuf Wibisono <yusuf.wibisono-guest@moladin.com>
2024-04-13 00:20:54 +0700

    add deploy sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,1 @@
-#!/bin/bash
-
-# Compile Go code
-go build -o main
-
-# Reload systemd
-sudo systemctl daemon-reload
-
-# Stop, start, enable, and check status of the goweb service
-sudo systemctl stop goweb
-sudo systemctl start goweb
-sudo systemctl enable goweb
-sudo systemctl status goweb
+sudo systemctl daemon-reload && sudo systemctl stop goweb && sudo systemctl start goweb && sudo systemctl enable goweb && sudo systemctl status goweb
